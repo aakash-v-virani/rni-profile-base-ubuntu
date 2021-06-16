84e4567b88ed979e2467dd2b4a35ad7c154f883f
aakash-v-virani <aakash.v.virani@intel.com>
2021-06-16 15:18:55 +0530

    Update pre.sh

--- pre.sh
+++ pre.sh
@@ -308,11 +308,9 @@ run "Configuring Image Database" \
 
 while (! docker ps > /dev/null ); do sleep 0.5; done
 
-if [ ! -z "${param_docker_login_pass}" ]; then
-    run "Log in to a Docker registry" \
+run "Log in to a Docker registry" \
     "docker login -u ${param_docker_login_user} -p ${param_docker_login_pass}" \
     "$TMP/provisioning.log"
-fi
 
 # --- Begin Ubuntu Install Process ---
 run "Preparing Ubuntu ${param_ubuntuversion} installer" \
